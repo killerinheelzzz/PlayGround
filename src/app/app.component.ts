38626a3a36233c2e6fdad95e5d582518224dfa99
Kelsey Davis <kelsey.davis@ge.com>
2017-07-24 08:12:02 -0700

    my play ground, playing with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
-import { Component } from '@angular/core';
+
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+
+
+
+export class ListOfITems {
+    name: string;
+    id: number;
+}
+
+const GROCERYLIST: ListOfITems[] = [
+    { id: 1, name: 'Ice Cream' },
+    { id: 2, name: 'Bread' },
+    { id: 3, name: 'Jam' },
+    { id: 4, name: 'Banana' }
+  ];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +21,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @Output() itemToBeViewd = new EventEmitter<any>();
+
+  item = 'List Of Thing From the Grocery';
+
+  listOfItems = GROCERYLIST;
+  selectedGroceryItem: ListOfITems;
+
+  onselect(listOfItems: ListOfITems): void {
+    this.selectedGroceryItem = listOfItems;
+  }
+
+  ListOutput(listOfItems) {
+    return this.itemToBeViewd.emit(listOfItems);
+  }
 }
